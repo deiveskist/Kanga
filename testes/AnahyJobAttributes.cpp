cd8d27fa43aa90a35bd611c0d689c5fa1bfa2cd0
deiveskist <deivesmesquitakist@gmail.com>
2014-04-09 21:00:36 -0300

    Refactor to simplify

--- testes/AnahyJobAttributes.cpp
+++
@@ -1,7 +0,0 @@
-#include "AnahyJobAttributes.h"
-
-AnahyJobAttributes::AnahyJobAttributes(bool smart) {
-	_smart = smart;
-	initialized = false;
-	job_cost = NORMAL_COST;
-}
--- testes/definitions.h
+++
@@ -1,4 +0,0 @@
-class Job;
-
-typedef void*(*ParFunc)(void*);
-typedef unsigned int uint;
